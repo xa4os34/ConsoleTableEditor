cf5d958df39863d921591358c21014c873a0ce5f
xa4os34 <matyushag8907@gmail.com>
2022-01-04 19:01:59 +0500

    Added decoration settings for TableCell

--- src/ConsoleTableEditor/ConsoleTableEditor.Core/Table.cs
+++ src/ConsoleTableEditor/ConsoleTableEditor.Core/Table.cs
@@ -1,14 +1,150 @@
-﻿using System.Numerics;
+﻿using System.Globalization;
+using System.Numerics;
 using System.Text;
 
 namespace ConsoleTableEditor.Core
 {
     public sealed class Table
     {
+
+    }
+
+    public enum ValueType
+    {
+        Empty,
+        Null,
+        String,
+        Currency,
+        Number,
+        Expression,
+    }
+
+    public interface IValue
+    {
+        public ValueType Type { get; }
+        public object? Value { get; }
+        public bool IsNotNull => Value is not null;
     }
 
     public sealed class TableCell
     {
+        public Position Position { get; }
+        public IValue? Value { get; set; }
+        public IDecorationSettings DecorationSettings { get; set; } = new DefaultDecorationSettings();
+    }
+
+    public interface IDecorationSettings
+    {
+        public Border Border { get; }
+    }
+
+    public sealed class DefaultDecorationSettings : IDecorationSettings
+    {
+        public Border Border { get; } = new Border();
+    }
+
+    public sealed class Border
+    {
+        public BorderType TopBorder { get; set; }
+        public BorderType BottomBorder { get; set; }
+        public BorderType RightBorder { get; set; }
+        public BorderType LeftBorder { get; set; }
+    }
+
+    public enum BorderType
+    {
+        Default,
+        Thick,
+        VeryThick,
+    }
+
+    public interface IExpression : IValue
+    {
+
+    }
+
+    public sealed class Expression : IExpression
+    {
+        public ValueType Type => ValueType.Expression;
+
+        public object? Value { get; private set; }
+    }
+
+    public sealed class String : IValue
+    {
+        private readonly string _value;
+
+        public String(string value)
+        {
+            _value = value;
+        }
+
+        public string StringValue => _value;
+        public object? Value => _value;
+        public ValueType Type => ValueType.String;
+    }
+
+    public sealed class Currency : IValue
+    {
+        private readonly decimal _value;
+     
+        public Currency(decimal value, CultureInfo culture)
+        {
+            Culture = culture;
+            _value = value;
+        }
+
+        public CultureInfo Culture { get; }
+        public decimal NumberValue => _value;
+        public object? Value => _value;
+        public ValueType Type => ValueType.Currency;
+
+        public override string ToString()
+        {
+            return $"{_value}{Culture.NumberFormat.CurrencySymbol}";
+        }
+    }
+
+    public sealed class Number : IValue
+    {
+        private readonly double _value;
+
+        public Number(double value)
+        {
+            _value = value;
+        }
+
+        public double NumberValue => _value;
+        public object? Value => _value;
+        public ValueType Type => ValueType.Number;
+
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+    }
+
+    public sealed class Null : IValue
+    {
+        public ValueType Type => ValueType.Null;
+        public object? Value => null;
+
+        public override string ToString()
+        {
+            return "Null";
+        }
+    }
+
+    public sealed class Empty : IValue
+    {
+        public ValueType Type => ValueType.Empty;
+
+        public object? Value => null;
+
+        public override string ToString()
+        {
+            return string.Empty;
+        }
     }
 
     public struct Position
--- src/ConsoleTableEditor/ConsoleTableEditor/Program.cs
+++ src/ConsoleTableEditor/ConsoleTableEditor/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using ConsoleTableEditor.Core;
+using System.Globalization;
+
+Console.WriteLine(decimal.MaxValue);
+Console.WriteLine(float.MaxValue);
+Console.WriteLine(double.MaxValue);
+
+
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine(new Number(1.0d + i / 100d));
+    Console.WriteLine(new Currency(1.0m + i / 100m, new CultureInfo("US-us")));
+    Console.WriteLine(new Currency(1.0m + i / 100m, new CultureInfo("RU-ru")));
+    Console.WriteLine(new Currency(1.0m + i / 100m, new CultureInfo("JP-jp")));
+}
